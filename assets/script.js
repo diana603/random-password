17e8a345223780dd4d733e2d97307bb8c1e047e6
dianaz <stingray2091@gmail.com>
2020-04-18 20:23:47 -0400

    added random function and randompassword function

--- assets/script.js
+++ assets/script.js
@@ -26,19 +26,59 @@ function generatePassword() {
 
     } else {
         console.log(length)
-        var alphabet = prompt("Would you like a letter in your password? ")
+        var alphabet = confirm("Would you like a letters in your password? ")
         console.log(alphabet)
-        var upperAlpha = prompt("would you like a capital letter? ")
+        var upperAlpha = confirm("would you like a capital letters? ")
         console.log(upperAlpha)
-        var num = prompt("Would you like a number in your password? ")
+        var num = confirm("Would you like a numbers in your password? ")
         console.log(num)
-        var character = prompt("Would you like a special character?")
+        var character = confirm("Would you like a special characters?")
         console.log(character)
-
-
     }
+    var userObject = {
+        length: length,
+        lowerCase: alphabet,
+        upperCase: upperAlpha,
+        num: num,
+        character: character,
+    }
+    console.log(userObject);
 
+    var randomPassword = getRandom(userObject)
+
+    return randomPassword
 }
 
+function getRandom(userObject) {
+    var combinedPassword = ""
+    if (userObject.lowerCase) {
+        combinedPassword += alphabet;
+    }
+
+    if (userObject.upperAlpha) {
+        combinedPassword += upperAlpha;
+    }
+
+    if (userObject.num) {
+        combinedPassword += num;
+    }
+
+    if (userObject.character) {
+        combinedPassword += character;
+    }
+
+    var password = "";
+
+    for (var i = 1; i <= userObject.length; i++) {
+
+        console.log("for loop ", i);
+
+        var randomCharactNumber = Math.floor(Math.random() * combinedPassword.length);
+
+        password += combinedPassword.charAt(randomCharactNumber);
+
+    }
+    return password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
